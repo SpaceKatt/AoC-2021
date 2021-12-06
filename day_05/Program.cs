10b6e99a832d8710736cc205f5535cde9a082108
SpaceKatt <spacekattpoispin@gmail.com>
2021-12-05 20:00:06 -0800

    add day 05 stub

---
+++ day_05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AoC2021
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 04");
+        }
+    }
+}
