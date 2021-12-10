fb8387574ef283e567e62844980f837cc16dfbec
SpaceKatt <spacekattpoispin@gmail.com>
2021-12-09 19:13:34 -0800

    solve day 09 pt 2

--- day_09/Program.cs
+++ day_09/Program.cs
@@ -10,6 +10,8 @@ namespace Advent2021
 
             var smoke = new SmokeSimulation("smoke.csv");
             smoke.PrintLowPoints();
+            smoke.PrintBasinMap();
+            smoke.PrintBasinStats();
 
             Console.WriteLine("End Day 09");
         }
@@ -18,10 +20,14 @@ namespace Advent2021
     class SmokeSimulation
     {
         List<List<int>> HeightMap;
+        List<List<int>> BasinMap;
+
+        private static int NO_BASIN = 0;
 
         public SmokeSimulation(string heightMapConfigPath)
         {
             this.HeightMap = this.GenerateHeightMapFromConfig(heightMapConfigPath);
+            this.BasinMap = this.GenerateBasinMap(this.HeightMap);
         }
 
         public void PrintLowPoints()
@@ -37,6 +43,99 @@ namespace Advent2021
             }
             Console.WriteLine();
             Console.WriteLine($"Low point sum\t\t{lowPointSum}");
+        }
+
+        public void PrintBasinMap()
+        {
+            Console.WriteLine("Basinmap");
+            for (int i = 0; i < this.BasinMap.Count(); i++)
+            {
+                for (int j = 0; j < this.BasinMap[0].Count(); j++)
+                {
+                    Console.Write($"{this.BasinMap[i][j]:000}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintBasinStats()
+        {
+            var basinCounter = new Dictionary<int, int>();
+
+            foreach (var row in this.BasinMap)
+            {
+                foreach (var basinId in row)
+                {
+                    if (basinId == SmokeSimulation.NO_BASIN) continue;
+                    if (!basinCounter.ContainsKey(basinId)) basinCounter.Add(basinId, 0);
+                    basinCounter[basinId]++;
+                }
+            }
+
+            var sortedBasinSize = basinCounter.Values.ToList<int>();
+            sortedBasinSize.Sort();
+
+            Console.WriteLine("Basin sizes:");
+            foreach (var size in sortedBasinSize)
+            {
+                Console.WriteLine($"{size}");
+            }
+        }
+
+        private List<List<int>> GenerateBasinMap(List<List<int>> heightMap)
+        {
+            var basinMap = Utils.InitializeMatrix<int>(heightMap.Count(), heightMap[0].Count(), 0);
+            var marked = Utils.InitializeMatrix<bool>(heightMap.Count(), heightMap[0].Count(), false);
+            if (heightMap.Count() != marked.Count() || heightMap[0].Count() != heightMap[0].Count())
+                throw new Exception("Marked space should have same dimensions as height space");
+
+            int basinCount = 0;
+
+            for (int i = 0; i < heightMap.Count(); i++)
+            {
+                for (int j = 0; j < heightMap.Count(); j++)
+                {
+                    if (marked[i][j])
+                    {
+                        continue;
+                    }
+                    else if (heightMap[i][j] == 9)
+                    {
+                        marked[i][j] = true;
+                        basinMap[i][j] = SmokeSimulation.NO_BASIN;
+                    }
+                    else
+                    {
+                        basinCount += 1;
+                        this.FloodFillBasin(basinMap, heightMap, marked, basinCount, new Coordinate(i, j));
+                    }
+                }
+            }
+
+            return basinMap;
+        }
+
+        private void FloodFillBasin(List<List<int>> basinMap, List<List<int>> heightMap, List<List<bool>> marked, int basinId, Coordinate fillPoint)
+        {
+            if (fillPoint.X < 0 || fillPoint.X >= heightMap.Count() || fillPoint.Y < 0 || fillPoint.Y >= heightMap[0].Count())
+                return;
+            if (marked[fillPoint.X][fillPoint.Y])
+                return;
+
+            marked[fillPoint.X][fillPoint.Y] = true;
+
+            if (heightMap[fillPoint.X][fillPoint.Y] == 9)
+            {
+                basinMap[fillPoint.X][fillPoint.Y] = SmokeSimulation.NO_BASIN;
+            }
+            else
+            {
+                basinMap[fillPoint.X][fillPoint.Y] = basinId;
+                this.FloodFillBasin(basinMap, heightMap, marked, basinId, new Coordinate(fillPoint.X + 1, fillPoint.Y));
+                this.FloodFillBasin(basinMap, heightMap, marked, basinId, new Coordinate(fillPoint.X - 1, fillPoint.Y));
+                this.FloodFillBasin(basinMap, heightMap, marked, basinId, new Coordinate(fillPoint.X, fillPoint.Y + 1));
+                this.FloodFillBasin(basinMap, heightMap, marked, basinId, new Coordinate(fillPoint.X, fillPoint.Y - 1));
+            }
         }
 
         private List<Coordinate> FindLowPoints()
@@ -98,6 +197,26 @@ namespace Advent2021
         {
             this.X = X;
             this.Y = Y;
+        }
+    }
+
+    class Utils
+    {
+        public static List<List<T>> InitializeMatrix<T>(int rows, int columns, T defaultValue)
+        {
+            var matrix = new List<List<T>>();
+
+            var matrixRow = new List<T>();
+            for (int i = 0; i < columns; i++)
+            {
+                matrixRow.Add(defaultValue);
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                matrix.Add(new List<T>(matrixRow));
+            }
+            return matrix;
         }
     }
 }
