56460cfa654d16ef3cef9356794e87f8985d8afc
SpaceKatt <spacekattpoispin@gmail.com>
2021-12-11 21:08:11 -0800

    stub day 12

---
+++ day_12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Advent2021
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Day 12");
+            Console.WriteLine();
+
+            Console.WriteLine("End Day 12");
+        }
+    }
+}
